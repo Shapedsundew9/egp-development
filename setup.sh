9cb32308ca292ee4702936597d9db7e8e4a09111
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2024-03-02 18:57:22 +0000

    Latest

--- setup.sh
+++ setup.sh
@@ -1,8 +1,25 @@
-#!/usr/bin/env bash
+# Clone development repos as needed
 bash clone-repos.sh
 
+# Add aliases
 alias commit='find /workspaces -type d -name ".git" -execdir pwd \; -execdir git add -u \; -execdir git commit -m "Latest" \; -execdir git push \;'
 alias commitall='find /workspaces -type d -name ".git" -execdir pwd \; -execdir git add -A \; -execdir git commit -m "Latest" \; -execdir git push \;'
 alias status='find /workspaces -type d -name ".git" -execdir pwd \; -execdir git status \;'
 alias push='find /workspaces -type d -name ".git" -execdir pwd \; -execdir git push \;'
 alias pull='find /workspaces -type d -name ".git" -execdir pwd \; -execdir git pull \;'
+
+# Add all repos to python path
+REPOPATHS=""
+for repo in ${REPOS}; do
+  REPOPATHS="${REPOPATHS}:~/Projects/${repo}"
+done
+echo "export PYTHONPATH=." >> ~/.bashrc
+for repo in ${REPOS}; do
+  echo "export PYTHONPATH=\${PYTHONPATH}:~/Projects/${repo}" >> ~/.bash-ss
+  if [ ! -d ${repo} ]
+    then
+      git clone git@github.com:Shapedsundew9/${repo}.git
+      git checkout Latest
+      git pull
+    fi
+done
